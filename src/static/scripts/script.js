b03f86ef3bf1dec7f64bda7fcb2300a0a95643d0
Navel <kirillovgundyai@yandex.ru>
2024-02-02 00:25:08 +0300

    made half form

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,25 +1,48 @@
-const wrapper = document.querySelector('[data-wrapper]')
-const ListItem = document.querySelector('[data-item]')
-const number = document.querySelector('[data-number]')
+// const wrapper = document.querySelector('[data-wrapper]')
+// const ListItem = document.querySelector('[data-item]')
+// const number = document.querySelector('[data-number]')
+// const inputValue = document.querySelector("#name")
 
 
+// function say () {
+// 	console.log(inputValue)
+// 	console.log(345345)
+// }
 
 
-wrapper.addEventListener('mouseover', (event) => {
-	const item = +event.target.atributeList.contains('[data-item]')
-	console.log(item);
-	if (item === 1) {
-		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
-	} else if (item === 2) {
-		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
-	}
+// let timerId = setInterval(() => console.log(inputValue), 1000);
+
+
+// wrapper.addEventListener('mouseover', (event) => {
+// 	const item = +event.target.atributeList.contains('[data-item]')
+// 	console.log(item);
+// 	if (item === 1) {
+// 		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
+// 	} else if (item === 2) {
+// 		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
+// 	}
+// })
+
+// function passHoverPattern (num) {
+// 	return `					
+// 	<div data-index="${num}" class="gallery__fone">
+// 		<button class="gallery__more button">Показать ещё</button>
+// 	</div>`
+// }
+
+
+// const button = document.querySelector("[data-button]")
+
+
+const mainForm = document.forms.pupu
+
+const mainFormInput = mainForm.user
+const mainFormTextarea = mainForm.tel
+
+mainForm.addEventListener("submit", ()=> {
+	alert(mainFormInput.value + '    ' + mainFormTextarea.value)
 })
 
-function passHoverPattern (num) {
-	return `					
-	<div data-index="${num}" class="gallery__fone">
-		<button class="gallery__more button">Показать ещё</button>
-	</div>`
-}
 
-
+// console.log(mainFormInput.value)
+// console.log(mainFormTextarea.value)
