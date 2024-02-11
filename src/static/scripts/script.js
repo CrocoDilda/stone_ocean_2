e0a69b39066d1ff2fbb7d9f11c6965b82dbbf73d
Navel <kirillovgundyai@yandex.ru>
2024-02-11 22:39:48 +0300

    finished layout

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,97 +1,40 @@
-// const wrapper = document.querySelector('[data-wrapper]')
-// const ListItem = document.querySelector('[data-item]')
-// const number = document.querySelector('[data-number]')
-// const inputValue = document.querySelector("#name")
+
+const servicesBtn = document.querySelector('[data-services-btn]')
+const sercives = document.querySelector('[data-services]')
+const advantagesBtn = document.querySelector('[data-advantages-btn')
+const advantages = document.querySelector('[data-advantages]')
+const galleryBtn = document.querySelector('[data-gallery-btn]')
+const gallery = document.querySelector('[data-gallery]')
+const contactsBtn = document.querySelector('[data-contacts-btn]')
+const contacts = document.querySelector('[data-contacts]')
+
+const hiddenBlock = document.querySelector('[data-hidden]')
+const hiddenBtn = document.querySelector('[data-hiddenBtn]')
+const closedBtn = document.querySelector('[data-closedBtn]')
+const body = document.querySelector('[data-body]')
 
 
-// function say () {
-// 	console.log(inputValue)
-// 	console.log(345345)
-// }
+servicesBtn.addEventListener('click', e => {scroll(sercives)})
+advantagesBtn.addEventListener('click', e => {scroll(advantages)})
+galleryBtn.addEventListener('click', e => {scroll(gallery)})
+contactsBtn.addEventListener('click', e => {scroll(contacts)})
 
-
-// let timerId = setInterval(() => console.log(inputValue), 1000);
-
-
-// wrapper.addEventListener('mouseover', (event) => {
-// 	const item = +event.target.atributeList.contains('[data-item]')
-// 	console.log(item);
-// 	if (item === 1) {
-// 		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
-// 	} else if (item === 2) {
-// 		ListItem.insertAdjacentHTML ('beforeend', passHoverPattern(item))
-// 	}
-// })
-
-// function passHoverPattern (num) {
-// 	return `					
-// 	<div data-index="${num}" class="gallery__fone">
-// 		<button class="gallery__more button">Показать ещё</button>
-// 	</div>`
-// }
-
-
-// const button = document.querySelector("[data-button]")
-
-
-
-// const mainForm = document.forms.pupu
-
-// const inputRadio = document.querySelector('[data-inputAtr]')
-
-// console.log(inputRadio.target)
-
-// const mainFormInput = mainForm.user
-// const mainFormTextarea = mainForm.tel
-// const inputRadioBtn = mainForm.radioButton
-
-// mainForm.addEventListener("submit", ()=> {
-// 	console.log(inputRadioBtn[0].checked)
-// 	console.log(inputRadioBtn[1].checked)
-// 	console.log(inputRadioBtn[2].checked)
-// 	alert(mainFormInput.value + '    ' + mainFormTextarea.value)
-// })
-
-
-// console.log(mainFormInput.value)
-// console.log(mainFormTextarea.value)
-
-
-
-// function sendEmail() {
-
-
-// var name = document.getElementsByName("user")[0].value;
-
-
-// var email = document.getElementsByName("tel")[0].value;
-
-
-// // const message = document.getElementsByName("message")[0].value;
-
-
-// var subject = "Новое сообщение от Stone Ocean"
-
-
-// var body = "Имя: " + name + " ";
-// body += "Email: " + email + " ";
-// body += "Сообщение:" + message;
-// return window.open("mailto:misano4955@giratex.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body))
-// console.log(body)
-// alert()
-// }
-
-let form = document.querySelector('pupu');
-const userName = document.querySelector('#name')
-const userNumber = document.querySelector('#call')
-const buttonInp = document.querySelector("#send")
-console.log(serialize('pupu'))
-
-
-buttonInp.onclick = function (event) {
-	event.preventDefault ()
-	console.log(userName.value)
-	console.log(userNumber.value)
+function scroll(section) {
+		section.scrollIntoView({ 
+		block: 'start', // к ближайшей границе экрана
+		behavior: 'smooth', // и плавно 
+	})
 }
 
+hiddenBtn.addEventListener('click', () => {
+	hiddenBlock.classList.remove('block_hidden')
+	hiddenBlock.classList.add('absolute')
+	body.classList.add('body--hidden')
+})
+
+closedBtn.addEventListener('click', () => {
+	hiddenBlock.classList.remove('absolute')
+	hiddenBlock.classList.add('block_hidden')
+	body.classList.remove('body--hidden')
+})
 
