9fd3ebb36919d09715b571c759c51a51055a24c2
CrocoDilda <cucuprum4k@gmail.com>
2024-03-19 12:47:02 +0300

    commit before changes

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,40 +1,22 @@
-
-const servicesBtn = document.querySelector('[data-services-btn]')
-const sercives = document.querySelector('[data-services]')
-const advantagesBtn = document.querySelector('[data-advantages-btn')
-const advantages = document.querySelector('[data-advantages]')
-const galleryBtn = document.querySelector('[data-gallery-btn]')
-const gallery = document.querySelector('[data-gallery]')
-const contactsBtn = document.querySelector('[data-contacts-btn]')
-const contacts = document.querySelector('[data-contacts]')
-
 const hiddenBlock = document.querySelector('[data-hidden]')
-const hiddenBtn = document.querySelector('[data-hiddenBtn]')
-const closedBtn = document.querySelector('[data-closedBtn]')
-const body = document.querySelector('[data-body]')
+const hiddenButton = document.querySelectorAll('[data-hiddenButton]')
+const closedButton = document.querySelector('[data-closedButton]')
+const body = document.querySelector('body')
+const inputs = document.querySelectorAll('input')
 
 
-servicesBtn.addEventListener('click', e => {scroll(sercives)})
-advantagesBtn.addEventListener('click', e => {scroll(advantages)})
-galleryBtn.addEventListener('click', e => {scroll(gallery)})
-contactsBtn.addEventListener('click', e => {scroll(contacts)})
-
-function scroll(section) {
-		section.scrollIntoView({ 
-		block: 'start', // к ближайшей границе экрана
-		behavior: 'smooth', // и плавно 
+hiddenBlock.style.transition = '.2s'
+hiddenButton.forEach(element => {
+	element.addEventListener('click', () => {
+		body.style.overflowY = 'hidden'
+		hiddenBlock.className = 'absolute'
 	})
-}
+});
 
-hiddenBtn.addEventListener('click', () => {
-	hiddenBlock.classList.remove('block_hidden')
-	hiddenBlock.classList.add('absolute')
-	body.classList.add('body--hidden')
+closedButton.addEventListener('click', () => {
+	hiddenBlock.className = 'block_hidden'
+	body.style.overflowY = 'scroll'
 })
 
-closedBtn.addEventListener('click', () => {
-	hiddenBlock.classList.remove('absolute')
-	hiddenBlock.classList.add('block_hidden')
-	body.classList.remove('body--hidden')
-})
-
+console.dir()
+document.forms[1].name.placeholder = 'adsf'
